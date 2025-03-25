21030603086e6b9ba0b504106c2a12df985b9a20
Suho34 <suhaibnisar01@gmail.com>
2025-03-25 18:35:31 +0530

    updated to include env

--- src/firebase.js
+++ src/firebase.js
@@ -17,6 +17,7 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+console.log("VITE_FIREBASE_API_KEY:", import.meta.env.VITE_FIREBASE_API_KEY);
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
